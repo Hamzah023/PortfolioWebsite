f79e6aa0583bf648ed1b247cd933f586c6a003f7
Hamzah Imran <hamzah@Hamzahs-MacBook-Air.local>
2024-12-16 02:23:55 -0500

    initial

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import pfp from './assets/pfp.jpeg';
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <a target="_blank" rel="noopener">
+          <img src={pfp} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Welcome to my portfolio.</h1>
+      <div className="card">
+        <button>
+          scroll down
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
